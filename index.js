5e65061f1edc7c691e4ad8d2b4f20e9c31dba1f7
igorgames-htm <sigmasigma14653@gmail.com>
2025-08-22 23:46:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get("/v1/api/getimgfromunid", async (req, res) => {
 });
 
 app.get("/", async (req, res) => {
-  res.send("Hello World");
+  res.json(JSON.parse({ message: "Hello World" });
 });
 
 app.get("/v1/api/gametext", async (req, res) => {
